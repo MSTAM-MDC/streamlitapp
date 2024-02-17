e7412786fd1d49accf3d906cebf5be7adf1ebdc5
MSTAM-MDC <99449410+MSTAM-MDC@users.noreply.github.com>
2024-02-16 20:19:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,6 @@ if st.button('Predict Sentiment'):
         st.write('Positive Sentiment')
     else:
         st.write('Negative Sentiment')
+tab1, tab2 = st.tabs(["About", "Why"])
+tab1.write("Sentiment analysis, also known as opinion mining, is the process of determining the sentiment or emotion expressed in a piece of text. It involves using natural language processing (NLP) techniques to analyze and classify subjective information as positive, negative, or neutral.")
+tab1.write("In summary, sentiment analysis empowers businesses to make informed decisions, enhance customer experiences, and safeguard their brand reputation.")
